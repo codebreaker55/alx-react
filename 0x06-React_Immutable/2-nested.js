db35d6292d954adf6e79a1a986dad44c354c4893
belal <belalismail252@gmail.com>
2024-09-11 22:57:39 +0300

    immutable

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-	const objectValue = fromJS(object);
+  const objectValue = fromJS(object);
 
-	return objectValue.getIn(array, undefined);
+  return objectValue.getIn(array, undefined);
 }
